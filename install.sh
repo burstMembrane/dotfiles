8d846b6514d4d126ea4ad1c3120bdfb52f82626d
Liam <liamfpower@gmail.com>
2024-10-17 22:21:47 +1100

    Feat: Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # update all submodules
 git submodule update --init --recursive
-home_dir_packages=("vim" "git" "zsh" "tmux")
-config_dir_packages=("nvim" "kitty")
+home_dir_packages=("vim" "git" "zsh" "tmux" "nvim")
 for package in "${home_dir_packages[@]}"
 do 
   if [ -d $package ]; then 
@@ -13,12 +12,3 @@ do
 fi
 done
 
-for package in "${config_dir_packages[@]}"
-do 
-  if [ -d $package ]; then 
-    echo "Installing config for $package"
-    echo "stow -t $HOME/.config $package"
-  else
-    echo "No config for $package"
-fi
-done
