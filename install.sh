553c6139d792ea1017ecc654b9de0011612231e5
Liam <liamfpower@gmail.com>
2024-10-18 10:09:31 +1100

    Feat: Add neovim module and kitty conf

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # update all submodules
 git submodule update --init --recursive
-home_dir_packages=("vim" "git" "zsh" "tmux" "nvim")
-for package in "${home_dir_packages[@]}"
+
+packages=(`ls -d */ | tr "\n" " " | sed 's/\///g'`)
+echo "Installing config for selected packages $packages"
+printf '%s\n' "${packages[@]}"
+for package in "${packages[@]}"
 do 
   if [ -d $package ]; then 
     echo "Installing config for $package"
-    echo "stow -t $HOME $package"
+    stow -t $HOME $package
   else
     echo "No config for $package"
 fi
