661dd428d18225ad8326de28e31b25c9d4b1da31
Liam <liamfpower@gmail.com>
2024-10-18 12:55:40 +1100

    Add scripts and uninstall.sh as well as zed conf

--- uninstall.sh
+++ uninstall.sh
@@ -8,11 +8,7 @@ echo "Installing config for selected packages $packages"
 printf '%s\n' "${packages[@]}"
 for package in "${packages[@]}"
 do 
-  if [ -d $package ]; then 
     echo "Cleaning config for $package"
     stow -D $package
-  else
-    echo "No config for $package"
-fi
 done
 
